7d62c937c4ca18184eb3f0d263be1551bb6d1858
Vanessa Lima <1123066+vanessagl2@users.noreply.github.com>
2020-08-01 15:51:47 -0300

    Use spans to organise the pictures grid

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
-    render() {
-        const {alt_description, urls} = this.props.image;
+    constructor(props) {
+        super(props);
+
+        this.state = {spans: 0};
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.height;
         
-        return <div>
+        const spansForImage = Math.ceil((height/10));
+
+        this.setState({spans: spansForImage});
+    }
+
+    render() {
+        const { alt_description, urls } = this.props.image;
+
+        return <div style={{gridRowEnd: `span ${this.state.spans}`}}>
             <img
+                ref={this.imageRef}
                 alt={alt_description}
                 src={urls.regular}
             />
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,10 +5,10 @@ import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <ImageCard key={image.id} image={image}/>
-    });
+            return <ImageCard key={image.id} image={image} />
+        });
 
-    return <div className="image-list">{images}</div>;
+        return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
